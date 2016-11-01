d4b2d8f403bbf7f840ee96bff999df646a1ae34b
guillaume <sibyllin04@gmail.com>
2016-11-01 11:45:18 -0700

    renamed and restructured repo

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 
 from distutils.core import setup
 
-setup(name='fineng',
+setup(name='financial-engineering',
       version='1.0',
       description='Financial Engineering Utilities',
       author='DrG',
       author_email='guillaume@stratifi.com',
-      url='https://github.com/StratiFi/fineng/',
+      url='https://github.com/StratiFi/financial-engineering/',
       packages=['fineng'],
      )
