8a7fa3d5e863ac0217ad4a2278b9b858365143f8
guillaume <sibyllin04@gmail.com>
2016-11-01 11:31:14 -0700

    refactored package

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='fineng',
+      version='1.0',
+      description='Financial Engineering Utilities',
+      author='DrG',
+      author_email='guillaume@stratifi.com',
+      url='https://github.com/StratiFi/fineng/',
+      packages=['metrics'],
+     )
